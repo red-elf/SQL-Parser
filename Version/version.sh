136cfb29d6dfce2058a2c043f1c70a1462bc6e04
Milos Vasic <milos85vasic@gmail.com>
2022-08-15 19:48:23 +0300

    Version.

--- Version/version.sh
+++ Version/version.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export VERSIONABLE_SNAPSHOT=true
-export VERSIONABLE_NAME="SQL Pareser"
+export VERSIONABLE_NAME="SQL Parser"
 export VERSIONABLE_VERSION_PRIMARY="1"
 export VERSIONABLE_VERSION_SECONDARY="0"
 export VERSIONABLE_VERSION_PATCH="0"
+export VERSIONABLE_HOMEPAGE="git@github.com:red-elf/SQL-Parser.git"
+export VERSIONABLE_DESCRIPTION="The SQL parser for the C++ projects by Red Elf."
