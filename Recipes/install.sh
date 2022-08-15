883c7000a874f429fcf0c4e362acd2ff2b4f090c
Milos Vasic <milos85vasic@gmail.com>
2022-08-15 19:56:08 +0300

    Install recipe.

--- Recipes/install.sh
+++ Recipes/install.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-echo "To be implemented!"
-exit 1
+VERSION_SCRIPT="Version/version.sh"
+
+if ! test -e "$VERSION_SCRIPT"; then
+
+  echo "ERROR: The version script was not found at path: '$VERSION_SCRIPT'"
+  exit 1
+fi
+
+# shellcheck disable=SC1090
+. "$VERSION_SCRIPT" && \
+  echo "Installing the '$VERSIONABLE_NAME' target, please wait" && \
+  cd "Library" && \
+  git reset --hard HEAD && \
+  make -j "$(nproc)" && \
+  sudo make install && \
+  echo "The '$VERSIONABLE_NAME' target has been installed with success" && \
+  git reset --hard HEAD
+
+# Install log example:
+#
+# cp libsqlparser.so /usr/local/lib/libsqlparser.so
+# rm -rf /usr/local/include/hsql
+# cp -r src /usr/local/include/hsql
+# find /usr/local/include/hsql -not -name '*.h' -type f | xargs rm
